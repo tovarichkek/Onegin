3250ffe70884899e5e85b361981d252f7197c172
tovarichkek <90504647+tovarichkek@users.noreply.github.com>
2023-09-22 02:55:58 +0300

    backup

--- onegin_main.cpp
+++ onegin_main.cpp
@@ -7,7 +7,7 @@
 
 int main(){
 
-
+/*
     FILE* file = fopen("ex.txt", "r");
     char* s = file_to_str(file);
     int count_of_strs = 1;
@@ -26,11 +26,25 @@ int main(){
         }
     }
 
-    hoar_sort(sizeof(char*), mass_of_onegin_strs, mass_of_onegin_strs + count_of_strs, comp_str);
-    for(int i = 0; i < count_of_strs; i++){
+    bubble_sort(sizeof(char*), mass_of_onegin_strs, mass_of_onegin_strs + count_of_strs-1, comp_str);
+    for(int i = 0; i < count_of_strs-1; i++){
         puts(mass_of_onegin_strs[i]);
     }
     free(mass_of_onegin_strs);
     free(s);
+*/
+    const char** mass_of_p_str = (const char**)calloc(5, sizeof(char*));
+    mass_of_p_str[0] = "csdv";
+    mass_of_p_str[1] = "dcd";
+    mass_of_p_str[2] = "evfs";
+    mass_of_p_str[3] = "bvfs";
+    mass_of_p_str[4] = "avfs";
+    bubble_sort(sizeof(char*), mass_of_p_str, mass_of_p_str + 5, comp_str);
+    for(int i = 0; i < 5; i++){
+        printf("%s ", mass_of_p_str[i]);
+    }
+    printf("\n");
+    free(mass_of_p_str);
 
+    //printf("%d\n", comp_str("csdv", "bvfs"));
 }
